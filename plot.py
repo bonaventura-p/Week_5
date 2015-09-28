0fcaa39586ddcbb7c75d255a421852f3584e0825
bonaventura-p <bonaventurapacileo@gmail.com>
2015-09-28 15:56:52 +0200

    plotting

---
+++ plot.py
@@ -0,0 +1,10 @@
+import matplotlib.pyplot as plt
+import numpy as np
+import matplotlib.mlab as mlab
+mean = 0
+variance = 1
+# standard deviation
+sigma = np.sqrt(variance)
+x = np.linspace(-3,3,100)
+plt.plot(x, mlab.normpdf(x,mean,sigma))
+plt.show()
